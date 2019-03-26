44369e12c905ac45a00715a630a13e0686b79e36
moonheart <fzp1205561571@hotmail.com>
2019-03-26 22:04:09 +0800

    Unit test for MongoEntityStorage

--- test/DotnetSpider.Tests/Data/Storage/MongoEntityStorageTests.cs
+++ test/DotnetSpider.Tests/Data/Storage/MongoEntityStorageTests.cs
@@ -1,8 +1,12 @@
 using System;
+using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using DotnetSpider.Data;
 using DotnetSpider.Data.Storage;
 using DotnetSpider.Data.Storage.Model;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Driver;
 using Moq;
 using Xunit;
@@ -18,25 +22,42 @@ namespace DotnetSpider.Tests.Data.Storage
         /// 3. 数据存储是否正确
         /// </summary>
         [Fact]
-        public async Task Store()
+        public async Task Store_Should_Success()
         {
             var serviceProvider = Mock.Of<IServiceProvider>();
-            var mongoEntityStorage = new MongoEntityStorage("mongodb://db1.example.net,db2.example.net:2500/?replicaSet=test");
+
+            var mongoCollection = new Mock<IMongoCollection<BsonDocument>>();
+            
+            var mongoDatabase = new Mock<IMongoDatabase>();
+            mongoDatabase.Setup(d =>
+                    d.GetCollection<BsonDocument>(It.IsAny<string>(), It.IsAny<MongoCollectionSettings>()))
+                .Returns(mongoCollection.Object);
+
+            var mongoClient = new Mock<IMongoClient>();
+            mongoClient.Setup(d => d.GetDatabase(It.IsAny<string>(), It.IsAny<MongoDatabaseSettings>()))
+                .Returns(mongoDatabase.Object);
+            
+            var mongoEntityStorage = new MongoEntityStorage(mongoClient.Object);
+            
             var tableMetadata = new TableMetadata {Schema = new Schema("db", "table")};
             
             var dataFlowContext = new DataFlowContext(serviceProvider);
             dataFlowContext.Add("table", tableMetadata);
-            dataFlowContext.AddItem("table", new object[0]);
+            dataFlowContext.AddItem("table", new object[] {new Dictionary<string, object> {{"Name", "Value"}}});
             
             var result = await mongoEntityStorage.HandleAsync(dataFlowContext);
+            
             Assert.Equal(DataFlowResult.Success, result);
         }
+
 
         [Fact]
         public async Task Store_Empty_Should_Success()
         {
             var serviceProvider = Mock.Of<IServiceProvider>();
-            var mongoEntityStorage = new MongoEntityStorage("mongodb://db1.example.net,db2.example.net:2500/?replicaSet=test");
+            var mongoClient = new Mock<IMongoClient>();
+            var mongoEntityStorage = new MongoEntityStorage(mongoClient.Object);
+            
             var dataFlowContext = new DataFlowContext(serviceProvider);
             var result = await mongoEntityStorage.HandleAsync(dataFlowContext);
             Assert.Equal(DataFlowResult.Success, result);
