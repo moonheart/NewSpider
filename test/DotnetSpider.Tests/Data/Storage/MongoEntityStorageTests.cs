f4969e19a6d383bd91aa8cb0cd31da108c49a6db
moonheart <fzp1205561571@hotmail.com>
2019-03-26 21:18:11 +0800

    Add unit tests for MongoEntityStorage

--- test/DotnetSpider.Tests/Data/Storage/MongoEntityStorageTests.cs
+++ test/DotnetSpider.Tests/Data/Storage/MongoEntityStorageTests.cs
@@ -1,3 +1,10 @@
+using System;
+using System.Threading.Tasks;
+using DotnetSpider.Data;
+using DotnetSpider.Data.Storage;
+using DotnetSpider.Data.Storage.Model;
+using MongoDB.Driver;
+using Moq;
 using Xunit;
 
 namespace DotnetSpider.Tests.Data.Storage
@@ -11,9 +18,28 @@ namespace DotnetSpider.Tests.Data.Storage
         /// 3. 数据存储是否正确
         /// </summary>
         [Fact]
-        public void Store()
+        public async Task Store()
         {
-            // TODO
+            var serviceProvider = Mock.Of<IServiceProvider>();
+            var mongoEntityStorage = new MongoEntityStorage("mongodb://db1.example.net,db2.example.net:2500/?replicaSet=test");
+            var tableMetadata = new TableMetadata {Schema = new Schema("db", "table")};
+            
+            var dataFlowContext = new DataFlowContext(serviceProvider);
+            dataFlowContext.Add("table", tableMetadata);
+            dataFlowContext.AddItem("table", new object[0]);
+            
+            var result = await mongoEntityStorage.HandleAsync(dataFlowContext);
+            Assert.Equal(DataFlowResult.Success, result);
+        }
+
+        [Fact]
+        public async Task Store_Empty_Should_Success()
+        {
+            var serviceProvider = Mock.Of<IServiceProvider>();
+            var mongoEntityStorage = new MongoEntityStorage("mongodb://db1.example.net,db2.example.net:2500/?replicaSet=test");
+            var dataFlowContext = new DataFlowContext(serviceProvider);
+            var result = await mongoEntityStorage.HandleAsync(dataFlowContext);
+            Assert.Equal(DataFlowResult.Success, result);
         }
     }
 }
